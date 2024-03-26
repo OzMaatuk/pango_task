bb4b8791c6eb57e7eb4ca0150725b0d1b0cc47e3
Oz <ozmaatuk@gmail.com>
2024-03-26 20:14:01 +0200

    fix assertion helper

--- automation_framework/utilities/assertions_helpers.py
+++ automation_framework/utilities/assertions_helpers.py
@@ -1,19 +1,30 @@
 class AssertionHelper:
     VALID_KEYS = {
-        'coord': ['lon', 'lat'],
-        'weather': [{'id': None, 'main': None, 'description': None, 'icon': None}],
-        'base': None,
-        'main': ['temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity'],
-        'visibility': None,
-        'wind': ['speed', 'deg'],
-        'rain': ['1h'],
-        'clouds': ['all'],
-        'dt': None,
-        'sys': ['type', 'id', 'country', 'sunrise', 'sunset'],
-        'timezone': None,
-        'id': None,
-        'name': None,
-        'cod': None
+        "coord": {"lon": None, "lat": None},
+        "weather": {"id": None, "main": None, "description": None, "icon": None},
+        "base": None,
+        "main": {
+            "temp": None,
+            "feels_like": None,
+            "temp_min": None,
+            "temp_max": None,
+            "pressure": None,
+            "humidity": None,
+        },
+        "visibility": None,
+        "wind": {"speed": None, "deg": None},
+        "rain": {"1h": None},
+        "clouds": {"all": None},
+        "dt": None,
+        "sys": {
+            "country": None,
+            "sunrise": None,
+            "sunset": None,
+        },
+        "timezone": None,
+        "id": None,
+        "name": None,
+        "cod": None,
     }
 
     @staticmethod
@@ -22,19 +33,44 @@ class AssertionHelper:
 
     @staticmethod
     def _assert_keys(valid_keys, response):
-        for key, subkeys in valid_keys.items():
-            if key not in response:
-                raise AssertionError(f"Missing key in the response: {key}")
-            if isinstance(subkeys, list):
-                for subkey in subkeys:
-                    if isinstance(subkey, dict):
-                        AssertionHelper._assert_keys(subkey, response[key][0])
-                    else:
-                        if subkey not in response[key][0]:
-                            raise AssertionError(f"Missing key in the response: {subkey}")
-            elif isinstance(subkeys, dict):
-                AssertionHelper._assert_keys(subkeys, response[key])
+        for key in response:
+            if isinstance(response[key], dict):
+                AssertionHelper._assert_keys(response[key], valid_keys[key])
+            else:
+                if not key in valid_keys:
+                    raise AssertionError(f"{key} is not exist in response", key)
+
 
 # Usage:
-# response = {...}  # Your actual API response
+# response = {
+#     "coord": {"lon": -0.1257, "lat": 51.5085},
+#     "weather": [
+#         {"id": 500, "main": "Rain", "description": "light rain", "icon": "10d"}
+#     ],
+#     "base": "stations",
+#     "main": {
+#         "temp": 11.01,
+#         "feels_like": 10.33,
+#         "temp_min": 9.6,
+#         "temp_max": 12.06,
+#         "pressure": 984,
+#         "humidity": 83,
+#     },
+#     "visibility": 10000,
+#     "wind": {"speed": 3.09, "deg": 90},
+#     "rain": {"1h": 0.95},
+#     "clouds": {"all": 75},
+#     "dt": 1711470264,
+#     "sys": {
+#         "type": 2,
+#         "id": 2075535,
+#         "country": "GB",
+#         "sunrise": 1711432120,
+#         "sunset": 1711477397,
+#     },
+#     "timezone": 0,
+#     "id": 2643743,
+#     "name": "London",
+#     "cod": 200,
+# }  # Your actual API response
 # AssertionHelper.assert_keys(response)
