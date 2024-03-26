005e339d39ec8a252443add9f5d8a74be642a451
Oz <ozmaatuk@gmail.com>
2024-03-26 17:09:47 +0200

    using general function

--- automation_framework/utilities/api_helpers.py
+++ automation_framework/utilities/api_helpers.py
@@ -4,20 +4,24 @@ from configparser import ConfigParser
 
 class ApiHelper:
     configur = ConfigParser()
-    configur.read('../config/config.ini') 
+    configur.read('automation_framework\\config\\config.ini') 
     API_KEY=configur.get('API', 'API_KEY')
-    BASE_URL=configur.get('API', 'API_KEY')
+    BASE_URL=configur.get('API', 'BASE_URL')
 
-    def get_current_weather(self, city):
-        url = f"{self.BASE_URL}?q={city}&appid={self.API_KEY}"
+    def get_weather(self, url):
         print(url)
         response = requests.get(url)
         print(response)
         return response
 
-    def get_weather_data(self, city_id):
-            url = f"{self.BASE_URL}?id={city_id}&appid={self.API_KEY}"
-            print(url)
-            response = requests.get(url)
-            print(response)
-            return response
+    def get_current_weather(self, city):
+        url = f"{self.BASE_URL}?q={city}&appid={self.API_KEY}"
+        return self.get_weather(url)
+
+    def get_current_weather_celsius(self, city):
+        url = f"{self.BASE_URL}?q={city}&appid={self.API_KEY}&units=metric"
+        return self.get_weather(url)
+
+    def get_current_weather_by_id(self, city_id):
+        url = f"{self.BASE_URL}?id={city_id}&appid={self.API_KEY}"
+        return self.get_weather(url)
