7040233cf401ddeb40f57e58f4eda72161681354
Oz <ozmaatuk@gmail.com>
2024-03-26 12:01:21 +0200

    add get by id and use config file

--- automation_framework/utilities/api_helpers.py
+++ automation_framework/utilities/api_helpers.py
@@ -1,12 +1,23 @@
 import requests
+from configparser import ConfigParser
+
 
 class ApiHelper:
-    BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-    API_KEY = "f88d54cd8eddb5d1f23ff82a80b95fec"
-    
+    configur = ConfigParser()
+    configur.read('../config/config.ini') 
+    API_KEY=configur.get('API', 'API_KEY')
+    BASE_URL=configur.get('API', 'API_KEY')
+
     def get_current_weather(self, city):
         url = f"{self.BASE_URL}?q={city}&appid={self.API_KEY}"
         print(url)
         response = requests.get(url)
         print(response)
         return response
+
+    def get_weather_data(self, city_id):
+            url = f"{self.BASE_URL}?id={city_id}&appid={self.API_KEY}"
+            print(url)
+            response = requests.get(url)
+            print(response)
+            return response
